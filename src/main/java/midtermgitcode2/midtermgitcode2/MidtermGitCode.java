d5aee7841eafba3f507bdd549214f24e8bafdf43
dylan ghangash <ghangasd@sheridancollege.ca>
2025-10-30 09:07:44 -0400

    rev 3 local

--- src/main/java/midtermgitcode2/midtermgitcode2/MidtermGitCode.java
+++ src/main/java/midtermgitcode2/midtermgitcode2/MidtermGitCode.java
@@ -12,6 +12,6 @@ package midtermgitcode2.midtermgitcode2;
 public class MidtermGitCode {
 
     public static void main(String[] args) {
-        System.out.println("Midterm Git Test rev 2");
+        System.out.println("Midterm Git Test rev 3");
     }
 }
