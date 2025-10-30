cea0fe02564185d0fd5825fec90767f77ef4d7a5
dylan ghangash <ghangasd@sheridancollege.ca>
2025-10-30 09:06:53 -0400

    rev 1 local

---
+++ src/main/java/midtermgitcode2/midtermgitcode2/Attendance.java
@@ -0,0 +1,46 @@
+package midtermgitcode2.midtermgitcode2;
+
+public class Attendance {
+
+	private String status;
+	private String studentName;
+	private Long studentId;
+
+	/**
+	 * 
+	 * @param status
+	 */
+	public Attendance(String status) {
+		// TODO - implement Attendance.Attendance
+		throw new UnsupportedOperationException();
+	}
+
+	public String getStatus() {
+		return this.status;
+	}
+
+	/**
+	 * 
+	 * @param status
+	 */
+	public void updateAttendance(String status) {
+		// TODO - implement Attendance.updateAttendance
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param name
+	 * @param id
+	 */
+	public void addStudent(String name, String id) {
+		// TODO - implement Attendance.addStudent
+		throw new UnsupportedOperationException();
+	}
+
+	public String toString() {
+		// TODO - implement Attendance.toString
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/main/java/midtermgitcode2/midtermgitcode2/Student.java
@@ -0,0 +1,51 @@
+package midtermgitcode2.midtermgitcode2;
+
+public class Student {
+
+	private String name;
+	private Long id;
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public Student(String name) {
+		// TODO - implement Student.Student
+		throw new UnsupportedOperationException();
+	}
+
+	private void setName() {
+		// TODO - implement Student.setName
+		throw new UnsupportedOperationException();
+	}
+
+	private void setId() {
+		// TODO - implement Student.setId
+		throw new UnsupportedOperationException();
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getId() {
+		// TODO - implement Student.getId
+		throw new UnsupportedOperationException();
+	}
+
+	private void createStudent() {
+		// TODO - implement Student.createStudent
+		throw new UnsupportedOperationException();
+	}
+
+	private Long generateId() {
+		// TODO - implement Student.generateId
+		throw new UnsupportedOperationException();
+	}
+
+	public String toString() {
+		// TODO - implement Student.toString
+		throw new UnsupportedOperationException();
+	}
+
+}
